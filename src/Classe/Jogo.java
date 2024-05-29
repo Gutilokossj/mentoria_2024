77d144091c58ba54591db8a470cc774408e3f3a6
Gustavo Fernandes <106595131+Gutilokossj@users.noreply.github.com>
2024-05-28 23:05:49 -0300

    Criação atributo classe Jogo
    Teste PULL

--- src/Classe/Jogo.java
+++ src/Classe/Jogo.java
@@ -4,6 +4,7 @@ public class Jogo {
     private String nome;
     private String Data_Lancamento;
     private float preco;
+    private String Desenvolvedora_do_Jogo;
 
     public Jogo(String nome, String Data_Lancamento, float preco) {
         this.nome = nome;
