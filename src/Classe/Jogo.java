1638d50021944df8960f449b207d651ff309f6b9
Gustavo Henrique Braga Fernandes <gutilokossj12@gmail.com>
2024-05-28 23:02:27 -0300

    Ajustando construtores classe

--- src/Classe/Jogo.java
+++ src/Classe/Jogo.java
@@ -4,6 +4,12 @@ public class Jogo {
     private String nome;
     private String Data_Lancamento;
     private float preco;
+
+    public Jogo(String nome, String Data_Lancamento, float preco) {
+        this.nome = nome;
+        this.Data_Lancamento = Data_Lancamento;
+        this.preco = preco;
+    }
 }
 
 
